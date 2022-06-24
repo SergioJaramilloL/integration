37e8b1b17daea33b938cc7f2b4eccecd327e39cf
Sergio <sergio@bellpi.com>
2022-06-24 09:21:48 -0500

    otro intento

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const sum = require(".");
 
 describe("sum", () => {
-  it("should add two nu,bers correcctly", () => {
+  it("should add two numbers correcctly", () => {
     expect(sum(1, 2)).toBe(3);
     expect(sum(11, 4)).toBe(15);
     expect(sum(78, 2)).toBe(80);
